d9d73e1f2477dd402dea95cfab2f55ea84bcd4db
h1sonath <huuvinh0201@gmail.com>
2018-07-26 21:46:14 +0700

    nang cap game arcade

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Player {
 
     public Random random = new Random();
-    public int x[] = {80, 50, 80};
+    public int x[] = {80, 40, 80};
     public int y[] = {160, 80, 160};
 
     public int velocity;
